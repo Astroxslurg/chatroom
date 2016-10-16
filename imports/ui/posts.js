c80233735a1401a8a0d4a9bef688f22b9f0903cc
astroxslurg <kkrorkk@gmail.com>
2016-10-16 19:06:24 +0200

    add user to posted message

--- imports/ui/posts.js
+++ imports/ui/posts.js
@@ -12,10 +12,17 @@ Template.postsSubmit.events({
   'submit .posts-submit-form'(event) {
     event.preventDefault();
 
+    user = Meteor.user();
+    if(!user){
+      console.log("You need to be logged in to post messages");
+      return false;
+    }
+
     const text = event.target.text.value;
     // console.log(event.target);
 
     Posts.insert({
+      user,
       text
     });
 
