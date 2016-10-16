1c882259015bc73a2ecafda2452358c93c7cf0ce
astroxslurg <kkrorkk@gmail.com>
2016-10-16 17:49:27 +0200

    changed name from postsContainer to postsTemplate

--- imports/ui/posts.js
+++ imports/ui/posts.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import './posts.html';
 import { Posts } from '../api/posts.js';
 
-Template.postsContainer.helpers({
+Template.postsTemplate.helpers({
   posts: function () {
     return Posts.find({});
   },
